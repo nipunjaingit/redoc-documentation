c010ccd0ecb1c02528bf6e7766d177f38169fea2
Nipun <nipun@Nipuns-MacBook-Pro.local>
2022-02-02 15:31:08 +0530

    Test

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi/openapi.yaml && \
+npx redoc-cli bundle openapi.json && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html" && \
 # Add favicon
